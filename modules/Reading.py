b8e8a4db00822f42794d1f256c3c41433f7fe0f3
Albert Tang <tangalbert919@gmail.com>
2018-06-16 14:53:19 -0500

    Better way to handle the news

--- modules/Reading.py
+++ modules/Reading.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
-import requests
 import json
+from newsapi import NewsApiClient
+
 
 class Reading:
 
@@ -11,13 +12,17 @@ class Reading:
     @commands.command()
     async def news(self, msg):
         config = json.loads(open('config.json', 'r').read())
-        url = ('https://newsapi.org/v2/top-headlines?'
+        newsapi = NewsApiClient(api_key=config.get('newsapitoken'))
+        top_headlines = newsapi.get_top_headlines(language='en', country='us')
+        print(top_headlines)
+        await msg.send(top_headlines)
+        """url = ('https://newsapi.org/v2/top-headlines?'
                'country=us&'
                'apiKey={}'.format(config.get('newsapitoken')))
         response = requests.get(url)
         print(response.json())
         newsurl = response.json()
-        await msg.send(newsurl['url'])
+        await msg.send(newsurl['url'])"""
 
 
 def setup(bot):
