6c3267cb4d6b2f0705d36ed2c0277738c71dc7b9
Albert Tang <tangalbert919@gmail.com>
2018-06-17 15:52:55 -0500

    Add reload command for debugging.

---
+++ modules/Debug.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+class Debug:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def reload(self, ctx, *, module):
+        """Reloads a module."""
+        try:
+            self.bot.unload_extension(module)
+            self.bot.load_extension(module)
+        except Exception as e:
+            await ctx.send(f'```py\n{traceback.format_exc()}\n```')
+        else:
+            await ctx.send(':ok_hand:')
+
+
+def setup(bot):
+    bot.add_cog(Debug(bot))
