234cb8279857f1878e1cd6bd5a4568c1aa456804
tangalbert919 <tangalbert919@gmail.com>
2018-06-19 20:44:52 -0500

    Add moderation module

---
+++ modules/Moderation.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+class Moderation:
+	
+	def __init__(self, bot):
+		self.bot = bot
+		
+	@commands.command()
+    async def kick(self, ctx, user: discord.User, *, reason):
+        await ctx.send("This feature has not been built yet.")
+
+    @commands.command()
+    async def ban(self, ctx, user: discord.User, *, reason):
+        await ctx.send("This feature has not been built yet.")
+
+    @commands.command()
+    async def mute(self, ctx, user: discord.User, *, reason):
+        await ctx.send("This feature has not been built yet.")
+
+
+def setup(bot):
+    bot.add_cog(Moderation(bot))
