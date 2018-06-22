664d352b9306b95da7f58625c5f572470db57aac
Albert Tang <tangalbert919@gmail.com>
2018-06-22 10:42:39 -0500

    Create looping status task

--- core.py
+++ core.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import json
 import os
+import asyncio
+
 
 class WeirdnessBot(commands.Bot):
 
@@ -20,13 +22,11 @@ class WeirdnessBot(commands.Bot):
                 except:
                     print(f"The {name} module failed to load. Please repair it and load it.")
 
-
     async def on_ready(self):
         print('Logged in as')
         print(self.user.name)
         print(self.user.id)
-        await self.change_presence(activity=discord.Activity(name='The Testing Grounds', type=discord.ActivityType.playing))
-        
+        self.loop.create_task(self.status_task())
 
     async def on_message(self, message):
         if message.author == self.user:
@@ -36,6 +36,16 @@ class WeirdnessBot(commands.Bot):
     async def on_command_error(self, context, exception):
         if isinstance(exception, discord.ext.commands.errors.MissingRequiredArgument):
             await context.send("You're missing one or more required arguments.")
+
+    async def status_task(self):
+        await self.change_presence(activity=discord.Activity(name='Do \"$help\" for help', type=discord.ActivityType.playing))
+        await asyncio.sleep(30)
+        await self.change_presence(activity=discord.Activity(name='PyCharm Community', type=discord.ActivityType.playing))
+        await asyncio.sleep(30)
+        await self.change_presence(activity=discord.Activity(name='24K Magic', type=discord.ActivityType.listening))
+        await asyncio.sleep(30)
+        await self.change_presence(activity=discord.Activity(name='A whole lot of anime stuff', type=discord.ActivityType.streaming))
+        await asyncio.sleep(30)
 
 
 client = WeirdnessBot()
