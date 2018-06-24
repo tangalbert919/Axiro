b44562770cf6111749433795fcd6c6cf812e91c7
Albert Tang <tangalbert919@gmail.com>
2018-06-24 18:14:38 -0500

    Enhance Moderation module

--- modules/Moderation.py
+++ modules/Moderation.py
@@ -8,14 +8,37 @@ class Moderation:
         self.bot = bot
 
     @commands.command()
+    @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, user: discord.User, *, reason):
-        await ctx.send("This feature has not been built yet.")
+        try:
+            await ctx.message.guild.kick(user, reason=reason)
+        except Exception:
+            await ctx.send("Player kick failed.")
+            return
+        await ctx.send("Player {} has been kicked from the server.".format(user.name))
 
     @commands.command()
+    @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, user: discord.User, *, reason):
-        await ctx.send("This feature has not been built yet.")
+        try:
+            await ctx.message.guild.ban(user, reason=reason)
+        except Exception:
+            await ctx.send("I completely failed to ban that player.")
+            return
+        await ctx.send("Player {} has been banned from the server.".format(user.name))
 
     @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def unban(self, ctx, user, *, reason):
+        try:
+            await ctx.message.guild.unban(user, reason=reason)
+        except Exception:
+            await ctx.send("I completely failed to unban that player.")
+            return
+        await ctx.send("Player {} has been unbanned from the server.".format(user))
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
     async def mute(self, ctx, user: discord.User, *, reason):
         await ctx.send("This feature has not been built yet.")
 
