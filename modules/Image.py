5c843abbd32c556e51f92e94348ea00307b2ad78
tangalbert919 <tangalbert919@gmail.com>
2018-10-05 23:35:47 -0500

    Revamp konachan command

--- modules/Image.py
+++ modules/Image.py
@@ -1,8 +1,6 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
-from pybooru import Moebooru
-import json
 import random
 import requests
 import aiohttp
@@ -62,7 +60,7 @@ class Image:
                 image_found = False
                 while not image_found:
                     async with session.get('https://danbooru.donmai.us/posts/random.json?search{}'
-                                        .format(temp)) as resp:
+                                                   .format(temp)) as resp:
                         data = await resp.json()
                     if not "loli".lower() in data['tag_string'] or not "shota".lower() in data['tag_string']:
                         image_found = True
@@ -81,11 +79,24 @@ class Image:
         await context.send(embed=embed)
 
     @commands.command()
+    async def test(self, ctx):
+        url_list = []
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://konachan.com/post/index.json?tags=-status%3Adeleted&limit=100') as resp:
+                data = await resp.json()
+                for entry in data:
+                    print(entry['file_url'])
+                    url_list.append(entry['file_url'])
+                url = url_list[random.randint(0, 99)]
+                print("The following url has been selected: \n")
+                print(url)
+
+    @commands.command()
     @commands.cooldown(1, 5, BucketType.user)
     async def konachan(self, context, tags=None, rating=None):
         """Picks a random image from Konachan and displays it."""
-        """TODO: Clean up this mess."""
-        client = Moebooru('konachan', username=self.bot.config['konachanname'],
+        """This code is no longer being used as of Release 3."""
+        """client = Moebooru('konachan', username=self.bot.config['konachanname'],
                           password=self.bot.config['konachanpasswd'])
         image_found = False
         if context.message.channel.is_nsfw():
@@ -97,11 +108,6 @@ class Image:
                     id_number = random.randint(1, highest_id)
                     temp = self.repairJSON(
                         str(client.post_list(limit=1, tags="-status:deleted id:{}".format(id_number))))
-                    """async with aiohttp.ClientSession() as session:
-                        async with session.get('https://konachan.com/post.json') as resp:
-                            temp = await resp.json()
-                            post_int = random.randint(1, 100)
-                            temp = temp[post_int]"""
                 elif "safe".lower() in tags:
                     id_number = random.randint(1, highest_id)
                     temp = self.repairJSON(
@@ -136,12 +142,62 @@ class Image:
         if context.message.guild is not None:
             color = context.message.guild.me.color
         else:
+            color = discord.Colour.blurple()"""
+        # As of Release 3, this new code is being used.
+        url_list = []
+        if context.message.channel.is_nsfw():
+            if tags is None:
+                temp = "?tags=-status%3Adeleted+-loli+-shota&limit=100"
+            elif "safe".lower() in tags:
+                temp = "?tags=-status%3Adeleted+-loli+-shota+rating:s&limit=100"
+            elif "explicit".lower() in tags:
+                temp = "?tags=-status%3Adeleted+-loli+-shota+rating:e&limit=100"
+            elif "questionable".lower() in tags:
+                temp = "?tags=-status%3Adeleted+-loli+-shota+rating:q&limit=100"
+            elif "loli".lower() in tags:
+                await context.send("We can't show this as it violates Discord ToS.")
+                return
+            elif "shota".lower() in tags:
+                await context.send("We can't show this as it violates Discord ToS.")
+                return
+            else:
+                if rating is None:
+                    temp = "?tags=-status%3Adeleted+-loli+-shota+{}&limit=100".format(tags)
+                elif "safe".lower() in rating:
+                    temp = "?tags=-status%3Adeleted+-loli+-shota+{}+rating:s&limit=100".format(tags)
+                elif "explicit".lower() in rating:
+                    temp = "?tags=-status%3Adeleted+-loli+-shota+{}+rating:e&limit=100".format(tags)
+                elif "questionable".lower() in rating:
+                    temp = "?tags=-status%3Adeleted+-loli+-shota+{}+rating:q&limit=100".format(tags)
+                else:
+                    await context.send("Please specify a valid rating. "
+                                       "Valid ratings include questionable, explicit, and safe.")
+                    return
+            async with aiohttp.ClientSession() as session:
+                async with session.get('https://konachan.com/post/index.json{}'
+                                               .format(temp)) as resp:
+                    data = await resp.json()
+                    for entry in data:
+                        print(entry['file_url'])
+                        url_list.append(entry['file_url'])
+            url = url_list[random.randint(0, 99)]
+        else:
+            await context.send("You need to be in a NSFW channel to run this command.")
+            return
+        if context.message.guild is not None:
+            color = context.message.guild.me.color
+        else:
             color = discord.Colour.blurple()
         embed = discord.Embed(color=color, title="Image from Konachan!",
                               description="If you can't see the image, click the title.", url=url)
         embed.set_image(url=url)
         embed.set_footer(text="Powered by Konachan.")
         await context.send(embed=embed)
+
+    @commands.command()
+    @commands.cooldown(1, 5, BucketType.user)
+    async def yandere(self, ctx, tags=None, rating=None):
+        await ctx.send("This bot will be connected to yande.re soon.")
 
     def repairJSON(self, temp):
         temp = temp.replace("{\'", "{\"")
