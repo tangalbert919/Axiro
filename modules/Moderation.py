c1ce0fca3c44a637f5a524333e95613acffc1e05
tangalbert919 <tangalbert919@gmail.com>
2018-10-12 17:49:52 -0500

    Add mute command, remind owners about bot permissions

--- modules/Moderation.py
+++ modules/Moderation.py
@@ -12,6 +12,9 @@ class Moderation:
     @commands.has_permissions(kick_members=True)
     @commands.cooldown(1, 5, BucketType.user)
     async def kick(self, ctx, user: discord.User, *, reason: str):
+        if not ctx.message.channel.permissions_for(ctx.message.author.guild.me).kick_members:
+            await ctx.send(":x: I do not have permission to kick players.")
+            return
         try:
             await ctx.message.guild.kick(user, reason=reason)
         except Exception:
@@ -23,6 +26,9 @@ class Moderation:
     @commands.has_permissions(ban_members=True)
     @commands.cooldown(1, 5, BucketType.user)
     async def ban(self, ctx, user: discord.User, *, reason: str):
+        if not ctx.message.channel.permissions_for(ctx.message.author.guild.me).ban_members:
+            await ctx.send(":x: I do not have permission to ban players.")
+            return
         try:
             await ctx.message.guild.ban(user, reason=reason)
         except Exception:
@@ -44,8 +50,16 @@ class Moderation:
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     @commands.cooldown(1, 5, BucketType.user)
-    async def mute(self, ctx, user: discord.User, *, reason: str):
-        await ctx.send("This feature has not been built yet.")
+    async def mute(self, ctx, user: discord.Member):
+        if not ctx.message.channel.permissions_for(ctx.message.author.guild.me).manage_roles:
+            await ctx.send(":x: I do not have permission to manage roles.")
+            return
+        try:
+            await ctx.message.channel.category.set_permissions(user, send_messages=False)
+        except Exception:
+            await ctx.send("I was unable to mute that player.")
+            return
+        await ctx.send(":white_check_mark: Player {} has been muted.".format(user.display_name))
 
 
 def setup(bot):
