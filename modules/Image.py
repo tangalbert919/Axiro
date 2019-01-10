b9acd6eed9c5f8059a458c752664596f2a63fb33
tangalbert919 <tangalbert919@gmail.com>
2019-01-09 18:00:11 -0600

    Fix the new logic

--- modules/Image.py
+++ modules/Image.py
@@ -29,48 +29,43 @@ class Image:
     async def danbooru(self, context, tags=None, secondtag=None):
         """Posts an image directly from Project Danbooru."""
         if context.message.channel.is_nsfw():
-            safe_rating = False
-            questionable_rating = False
-            explicit_rating = False
+            rating = 0
             temp = "[-status]=deleted"
             # No tags
             if tags is None:
                 pass
+            # Tags
             else:
-                # Do rating checks
-                if "safe".lower() in tags or "safe".lower() in secondtag:
-                    safe_rating = True
-                elif "explicit".lower() in tags or "explicit".lower() in secondtag:
-                    explicit_rating = True
-                elif "questionable".lower() in tags or "questionable".lower() in secondtag:
-                    questionable_rating = True
-                # Check for tags we can't use
-                if "loli".lower() in tags or "loli".lower() in secondtag:
-                    if "shota".lower() in tags or "shota".lower() in secondtag:
+                if secondtag is None:
+                    rating = self.checktags(tags, "")
+                    if not self.nololitag(tags, ""):
                         await context.send("We can't show this as it violates Discord ToS.")
                         return
-                    await context.send("We can't show this as it violates Discord ToS.")
-                    return
+                else:
+                    rating = self.checktags(tags, secondtag)
+                    if not self.nololitag(tags, secondtag):
+                        await context.send("We can't show this as it violates Discord ToS.")
+                        return
                 # One tag
                 if secondtag is None:
-                    if not safe_rating and not questionable_rating and not explicit_rating:
+                    if rating == 0:
                         temp = temp + "&[tags]={}".format(tags)
                     else:
-                        temp = temp + "&[tags]={}".format(self.rating(safe_rating, questionable_rating, explicit_rating))
+                        temp = temp + "&[tags]={}".format(self.rating(rating))
                 # Two tags
                 else:
-                    if not safe_rating and not questionable_rating and not explicit_rating:
+                    if rating == 0:
                         temp = temp + "&[tags]={}+{}".format(tags, secondtag)
                     else:
                         if "safe".lower() in tags or "questionable".lower() in tags or "explicit".lower() in tags:
-                            temp = temp + "&[tags]={}+{}".format(self.rating(safe_rating, questionable_rating, explicit_rating), secondtag)
+                            temp = temp + "&[tags]={}+{}".format(self.rating(rating), secondtag)
                         else:
-                            temp = temp + "&[tags]={}+{}".format(tags, self.rating(safe_rating, questionable_rating, explicit_rating))
+                            temp = temp + "&[tags]={}+{}".format(tags, self.rating(rating))
             async with aiohttp.ClientSession() as session:
                 async with session.get('https://danbooru.donmai.us/posts/random.json?search{}'
                                                    .format(temp)) as resp:
                     data = await resp.json()
-                session.close()
+                await session.close()
             try:
                 url = data['file_url']
             except Exception:
@@ -224,13 +219,31 @@ class Image:
         return temp
 
     @staticmethod
-    def rating(safe, questionable, explicit):
-        if safe:
+    def rating(integer):
+        if integer == 1:
             return "rating:s"
-        elif questionable:
+        elif integer == 2:
             return "rating:q"
-        elif explicit:
+        elif integer == 3:
             return "rating:e"
+
+    @staticmethod
+    def checktags(tagone, tagtwo):
+        if "safe".lower() in tagone or "safe".lower() in tagtwo:
+            return 1
+        elif "questionable".lower() in tagone or "questionable" in tagtwo:
+            return 2
+        elif "explicit".lower() in tagone or "explicit" in tagtwo:
+            return 3
+        return 0
+
+    @staticmethod
+    def nololitag(tagone, tagtwo):
+        if "loli".lower() in tagone or "loli".lower() in tagtwo:
+            return False
+        if "shota".lower() in tagone or "shota".lower() in tagtwo:
+            return False
+        return True
 
 
 def setup(bot):
