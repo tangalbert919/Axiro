6250a6663c6e2e15539d3650f1829b2cbce0ca57
Albert Tang <tangalbert919@yahoo.com>
2022-01-10 15:55:34 -0600

    Add command modules

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using Discord;
+﻿using Axiro.Services;
+using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.IO;
 using System.Text.Json;
@@ -11,6 +14,8 @@ namespace Axiro
     class Config
     {
         public string DiscordToken { get; set; }
+        public string NewsAPIToken { get; set; }
+        public string TopGGToken { get; set; }
     }
     class Program
     {
@@ -19,38 +24,68 @@ namespace Axiro
         static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
 
-        public Program()
+        /*public Program()
         {
             _client = new DiscordSocketClient();
 
             _client.Log += LogAsync;
             _client.Ready += ReadyAsync;
             _client.MessageReceived += MessageReceivedAsync;
-        }
+        }*/
         public async Task MainAsync()
         {
-            // Load configuration JSON file.
-            string fileName = "config.json";
-            using FileStream stream = File.OpenRead(fileName);
-            Config config = await JsonSerializer.DeserializeAsync<Config>(stream);
+            // You should dispose a service provider created using ASP.NET
+            // when you are finished using it, at the end of your app's lifetime.
+            // If you use another dependency injection framework, you should inspect
+            // its documentation for the best way to do this.
+            using (var services = ConfigureServices())
+            {
+                var client = services.GetRequiredService<DiscordSocketClient>();
 
-            await _client.LoginAsync(TokenType.Bot, $"{config.DiscordToken}");
-            await Task.Delay(Timeout.Infinite);
+                client.Log += LogAsync;
+                //client.Ready += ReadyAsync;
+                services.GetRequiredService<CommandService>().Log += LogAsync;
+
+                string fileName = "config.json";
+                using FileStream stream = File.OpenRead(fileName);
+                Config config = await JsonSerializer.DeserializeAsync<Config>(stream);
+
+                // Tokens should be considered secret data and never hard-coded.
+                // We can read from the environment variable to avoid hard coding.
+                await client.LoginAsync(TokenType.Bot, $"{config.DiscordToken}");
+                await client.StartAsync();
+
+                // Here we initialize the logic required to register our commands.
+                await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+
+                await Task.Delay(Timeout.Infinite);
+            }
         }
         private Task LogAsync(LogMessage arg)
         {
-            throw new NotImplementedException();
+            Console.WriteLine(arg.ToString());
+            return Task.CompletedTask;
         }
 
-        private Task ReadyAsync()
+        /*private Task ReadyAsync()
         {
             Console.WriteLine($"{_client.CurrentUser} is now ready.");
             return Task.CompletedTask;
-        }
+        }*/
 
         private Task MessageReceivedAsync(SocketMessage arg)
         {
             throw new NotImplementedException();
+        }
+        private ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+                //.AddSingleton<HttpClient>()
+                //.AddSingleton<PictureService>()
+                .BuildServiceProvider();
         }
     }
 }
