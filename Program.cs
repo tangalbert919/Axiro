fa7d2eeca2a34d64b5bb342f92881e33c95de169
Albert Tang <tangalbert919@yahoo.com>
2022-01-10 17:04:49 -0600

    Use more advanced command handlers

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.IO;
@@ -22,7 +23,16 @@ namespace Axiro
         private readonly DiscordSocketClient _client;
 
         static void Main(string[] args)
-            => new Program().MainAsync().GetAwaiter().GetResult();
+        {
+            // One of the more flexable ways to access the configuration data is to use the Microsoft's Configuration model,
+            // this way we can avoid hard coding the environment secrets. I opted to use the Json and environment variable providers here.
+            IConfiguration config = new ConfigurationBuilder()
+                .AddEnvironmentVariables(prefix: "test_")
+                .AddJsonFile("appsettings.json", optional: true)
+                .Build();
+
+            MainAsync(config).GetAwaiter().GetResult();
+        }
 
         /*public Program()
         {
@@ -32,36 +42,36 @@ namespace Axiro
             _client.Ready += ReadyAsync;
             _client.MessageReceived += MessageReceivedAsync;
         }*/
-        public async Task MainAsync()
+        public static async Task MainAsync(IConfiguration config)
         {
             // You should dispose a service provider created using ASP.NET
             // when you are finished using it, at the end of your app's lifetime.
             // If you use another dependency injection framework, you should inspect
             // its documentation for the best way to do this.
-            using (var services = ConfigureServices())
-            {
-                var client = services.GetRequiredService<DiscordSocketClient>();
+            using var services = ConfigureServices(config);
+            
+            var client = services.GetRequiredService<DiscordSocketClient>();
 
-                client.Log += LogAsync;
-                //client.Ready += ReadyAsync;
-                services.GetRequiredService<CommandService>().Log += LogAsync;
+            client.Log += LogAsync;
+            //client.Ready += ReadyAsync;
+            services.GetRequiredService<CommandService>().Log += LogAsync;
 
-                string fileName = "config.json";
-                using FileStream stream = File.OpenRead(fileName);
-                Config config = await JsonSerializer.DeserializeAsync<Config>(stream);
+            /*string fileName = "config.json";
+            string filePath = Path.GetFullPath(fileName);
+            using FileStream stream = File.OpenRead(fileName);
+            Config config = await JsonSerializer.DeserializeAsync<Config>(stream);*/
 
-                // Tokens should be considered secret data and never hard-coded.
-                // We can read from the environment variable to avoid hard coding.
-                await client.LoginAsync(TokenType.Bot, $"{config.DiscordToken}");
-                await client.StartAsync();
+            // Tokens should be considered secret data and never hard-coded.
+            // We can read from the environment variable to avoid hard coding.
+            await client.LoginAsync(TokenType.Bot, config["DiscordToken"]);
+            await client.StartAsync();
 
-                // Here we initialize the logic required to register our commands.
-                await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+            // Here we initialize the logic required to register our commands.
+            await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
 
-                await Task.Delay(Timeout.Infinite);
-            }
+            await Task.Delay(Timeout.Infinite);
         }
-        private Task LogAsync(LogMessage arg)
+        private static Task LogAsync(LogMessage arg)
         {
             Console.WriteLine(arg.ToString());
             return Task.CompletedTask;
@@ -77,7 +87,7 @@ namespace Axiro
         {
             throw new NotImplementedException();
         }
-        private ServiceProvider ConfigureServices()
+        private static ServiceProvider ConfigureServices(IConfiguration config)
         {
             return new ServiceCollection()
                 .AddSingleton<DiscordSocketClient>()
