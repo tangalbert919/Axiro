4dce314f6a25a67239aa62ea1e1220b62b203c13
Albert Tang <tangalbert919@yahoo.com>
2022-01-09 23:23:38 -0600

    Start rewrite

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Axiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
