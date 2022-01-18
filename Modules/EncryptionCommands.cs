4a9fc68f87868f33dfaacc55ac1143b4be2d6c29
Albert Tang <tangalbert919@yahoo.com>
2022-01-18 16:09:53 -0600

    Add more commands to encryption module

--- Modules/EncryptionCommands.cs
+++ Modules/EncryptionCommands.cs
@@ -22,12 +22,39 @@ namespace Axiro.Modules
         [SlashCommand("encode", "Encodes the message")]
         public async Task Encode(string hash, string message)
         {
-            string result = "";
+            string result;
             if (hash.ToLower().Equals("base64"))
                 result = Convert.ToBase64String(Encoding.UTF8.GetBytes(message));
             else
+            {
                 await RespondAsync("This hash is currently unavailable");
+                return;
+            }
             await RespondAsync(result);
+        }
+
+        [SlashCommand("decode", "Decodes the message")]
+        public async Task Decode(string hash, string message)
+        {
+            string result;
+            if (hash.ToLower().Equals("base64"))
+                result = Convert.FromBase64String(message).ToString();
+            else
+            {
+                await RespondAsync("This hash is currently unavailable");
+                return;
+            }
+            await RespondAsync(result);
+        }
+
+        [SlashCommand("reverse", "Reverses the message")]
+        public async Task Reverse(string message)
+        {
+            char[] temp = message.ToCharArray();
+            char[] result = new char[temp.Length];
+            for (int i = 0; i < temp.Length; i++)
+                result[i] = temp[temp.Length - 1 - i];
+            await RespondAsync(result.ToString());
         }
     }
 }
