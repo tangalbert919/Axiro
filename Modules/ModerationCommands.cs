0532bc50b9afdd4efe442102c846d4eafbd16da9
Albert Tang <tangalbert919@yahoo.com>
2022-01-18 18:36:46 -0600

    Add moderation commands

--- Modules/ModerationCommands.cs
+++ Modules/ModerationCommands.cs
@@ -1,4 +1,5 @@
 ﻿using Axiro.Services;
+using Discord;
 using Discord.Interactions;
 using System;
 using System.Collections.Generic;
@@ -17,6 +18,39 @@ namespace Axiro.Modules
         public ModerationCommands(CommandHandler handler)
         {
             _handler = handler;
+        }
+
+        [SlashCommand("kick", "Kick the specified user")]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        [RequireBotPermission(GuildPermission.KickMembers)]
+        public async Task Kick(IGuildUser user, [Summary(description: "Kick with specified reason")] string reason = null)
+        {
+            await user.KickAsync(reason);
+            await RespondAsync("User kicked.");
+        }
+
+        [SlashCommand("ban", "Bans the specified user")]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        [RequireBotPermission(GuildPermission.BanMembers)]
+        public async Task Ban(IGuildUser user, [Summary(description: "Prune messages for this many days")] int days = 0,
+            [Summary(description: "Ban with specified reason")] string reason = null)
+        {
+            await user.BanAsync(days, reason);
+            await RespondAsync("User banned.");
+        }
+
+        [SlashCommand("mute", "Mutes the specified user")]
+        public async Task Mute()
+        {
+            await RespondAsync("Coming soon");
+        }
+
+        [SlashCommand("prune", "Prunes messages")]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        [RequireBotPermission(GuildPermission.ManageMessages)]
+        public async Task Prune(int messages)
+        {
+            await RespondAsync("Coming soon");
         }
     }
 }
