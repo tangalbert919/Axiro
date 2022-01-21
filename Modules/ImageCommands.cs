4df4aac20d2ea362db09b3c4f51c96c13ebad058
Albert Tang <tangalbert919@yahoo.com>
2022-01-20 23:47:30 -0600

    Complete neko command

--- Modules/ImageCommands.cs
+++ Modules/ImageCommands.cs
@@ -1,13 +1,10 @@
 ﻿using Axiro.Services;
+using Discord;
 using Discord.Interactions;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Data;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Axiro.Modules
@@ -24,37 +21,50 @@ namespace Axiro.Modules
             _handler = handler;
         }
 
+        private class NekoAPI
+        {
+            public string url { get; set; }
+        }
+
         [SlashCommand("neko", "Get a neko image")]
         public async Task Neko()
         {
             client = new();
             StreamReader reader = new(await client.GetStreamAsync("https://nekos.life/api/v2/img/neko"));
 
-            DataSet set = JsonConvert.DeserializeObject<DataSet>(reader.ReadToEnd());
-            DataTable dt = set.Tables[0];
-            await RespondAsync("Coming soon");
+            NekoAPI result = JsonConvert.DeserializeObject<NekoAPI>(reader.ReadToEnd());
+            EmbedBuilder builder = new();
+            builder.Title = "From nekos.life";
+            builder.ImageUrl = result.url;
+            builder.Footer = new EmbedFooterBuilder().WithText("Requested by " + Context.User.Username);
+            await SendEmbed(builder.Build());
         }
 
         // TODO: Find some image boards that aren't anime.
         [SlashCommand("danbooru", "Get an image from Project Danbooru")]
         [RequireNsfw]
-        public async Task Danbooru([Summary(description: "Search with this tag")]string tag = null)
+        public async Task Danbooru([Summary(description: "Search with this tag")] string tag = null)
         {
             await RespondAsync("Coming soon");
         }
 
         [SlashCommand("konachan", "Get an image from Konachan")]
         [RequireNsfw]
-        public async Task Konachan([Summary(description: "Search with this tag")]string tag = null)
+        public async Task Konachan([Summary(description: "Search with this tag")] string tag = null)
         {
             await RespondAsync("Coming soon");
         }
 
         [SlashCommand("yandere", "Get an image from Yande.re")]
         [RequireNsfw]
-        public async Task Yandere([Summary(description: "Search with this tag")]string tag = null)
+        public async Task Yandere([Summary(description: "Search with this tag")] string tag = null)
         {
             await RespondAsync("Coming soon");
+        }
+
+        public async Task SendEmbed(Embed embed)
+        {
+            await RespondAsync(null, null, false, false, null, null, null, embed);
         }
     }
 }
