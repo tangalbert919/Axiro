3482ec27b4b9edd7c96922bd7e4157b5f84955a1
Albert Tang <tangalbert919@yahoo.com>
2022-01-24 15:41:21 -0600

    Actually build danbooru command
    Does not accept tags yet, will program in later.

--- Modules/ImageCommands.cs
+++ Modules/ImageCommands.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 using System;
 using System.IO;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Axiro.Modules
@@ -14,7 +15,6 @@ namespace Axiro.Modules
         public InteractionService Commands { get; set; }
 
         private CommandHandler _handler;
-        private HttpClient client;
 
         public ImageCommands(CommandHandler handler)
         {
@@ -29,7 +29,7 @@ namespace Axiro.Modules
         [SlashCommand("neko", "Get a neko image")]
         public async Task Neko()
         {
-            client = new();
+            HttpClient client = new();
             StreamReader reader = new(await client.GetStreamAsync("https://nekos.life/api/v2/img/neko"));
 
             NekoAPI result = JsonConvert.DeserializeObject<NekoAPI>(reader.ReadToEnd());
@@ -45,13 +45,29 @@ namespace Axiro.Modules
         [RequireNsfw]
         public async Task Danbooru([Summary(description: "Search with this tag")] string tag = null)
         {
-            await RespondAsync("Coming soon");
+            HttpClient client = new();
+
+            JsonDocument result = JsonDocument.ParseAsync(
+                client.GetStreamAsync("https://danbooru.donmai.us/posts/random.json").Result).Result;
+#if DEBUG
+            Console.WriteLine(result.RootElement);
+            Console.WriteLine(result.RootElement.GetProperty("id").GetUInt32());
+#endif
+            EmbedBuilder builder = new();
+            builder.Title = "From Project Danbooru";
+            builder.ImageUrl = result.RootElement.GetProperty("file_url").GetString();
+            builder.Footer = new EmbedFooterBuilder().WithText("Requested by " + Context.User.Username);
+            await SendEmbed(builder.Build());
         }
 
         [SlashCommand("konachan", "Get an image from Konachan")]
         [RequireNsfw]
         public async Task Konachan([Summary(description: "Search with this tag")] string tag = null)
         {
+            /*HttpClient client = new();
+
+            JsonDocument result = JsonDocument.ParseAsync(
+                client.GetStreamAsync("https://konachan.com/post/random").Result).Result;*/
             await RespondAsync("Coming soon");
         }
 
