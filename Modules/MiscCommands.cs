7e929352518d14008050aa564fb15bf591b00691
Albert Tang <tangalbert919@yahoo.com>
2022-02-08 14:30:14 -0600

    Add pig latin command

--- Modules/MiscCommands.cs
+++ Modules/MiscCommands.cs
@@ -14,6 +14,9 @@ namespace Axiro.Modules
 
         private CommandHandler _handler;
 
+        // This is for the pig latin command.
+        private readonly static char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
         public MiscCommands(CommandHandler handler)
         {
             _handler = handler;
@@ -80,6 +83,58 @@ namespace Axiro.Modules
         public async Task Chrome()
         {
             await RespondAsync("Cannot get current version of Chrome at this time.");
+        }
+
+        [SlashCommand("piglatin", "Translate something to pig latin.")]
+        public async Task PigLatin(string str)
+        {
+            // Copy all the words in the string to a collection.
+            string temp = str.ToLower();
+            List<string> process = new();
+            while (temp.Contains(' '))
+            {
+                process.Add(temp.Substring(0, temp.IndexOf(' ')));
+                temp = temp[(temp.IndexOf(' ') + 1)..];
+            }
+            // Add last word.
+            process.Add(temp);
+
+            // Translate every entry in the collection to pig latin.
+            // https://en.wikipedia.org/wiki/Pig_Latin
+            List<String> result = new();
+            foreach (string word in process)
+            {
+                Console.WriteLine($"DEBUG: Word to be processed is {word}");
+                char[] newword = word.ToCharArray();
+                // Word starts with a vowel
+                if (Array.IndexOf(vowels, newword[0]) > -1)
+                    temp = word + "yay";
+                // Word starts with a consonant.
+                else
+                {
+                    // Word does not start with consonant clusters.
+                    if (Array.IndexOf(vowels, newword[1]) > -1)
+                        temp = word[1..] + word[0] + "ay";
+                    // Word does start with consonant clusters.
+                    else
+                    {
+                        int index = 1;
+                        while (Array.IndexOf(vowels, newword[index]) == -1)
+                        {
+                            index++;
+                            if (index >= word.Length)
+                            {
+                                Console.WriteLine($"ERROR: Invalid word {word}");
+                                await RespondAsync("TRANSLATION FAILED!");
+                            }
+                        }
+                        temp = word[index..] + word[0..index] + "ay";
+                    }
+                }
+                result.Add(temp);
+                Console.WriteLine($"DEBUG: Word has been processed: {temp}");
+            }
+            await RespondAsync(result.ToString());
         }
     }
 }
